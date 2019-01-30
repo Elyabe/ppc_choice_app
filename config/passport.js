6364c9a8c7c9b27edb451e0527610a56fd4110af
elyabe <elyabe@outlook.com>
2019-01-30 11:22:34 -0200

    Reajuste: Import funcoes db

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
-// const mongoose = require('mongoose');
+const db = require('../routes/db_functions');
 const bcrypt = require('bcryptjs');
 
 // Load User model
