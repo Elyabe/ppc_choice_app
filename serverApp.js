a72f3451866b3f979ac792b91c5821c57ad181d8
elyabe <elyabe@outlook.com>
2019-02-02 18:43:48 -0200

    Limpeza do controller

--- serverApp.js
+++ serverApp.js
@@ -15,11 +15,6 @@ const PORT = process.env.PORT || 3000
 const comparison = require('./routes/controller')
 const index = require('./routes/controller')
 
-/*const bdFireRoute = require('./routes/db_firestore')
-
-require('dotenv').config()
-*/
-
 express()
 .use(
   session({
@@ -40,8 +35,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .engine('ejs', engine)
   .set('view engine', 'ejs')
-  // .get('/', (req, res) => res.render('pages/index'))
-  // .get('/', (req, res) => res.render('pages/home_construcao', { title: 'Seus putos' } ) )
   .get('/', index )
   .post('/login', index )
   .get('/logout', index )
@@ -50,16 +43,6 @@ express()
   .get('/comparison', comparison )
   .get('/getGrade/:idCurso', comparison )
   .get('/compare/:idCursoAtual/:idCursoAlvo', comparison )
-/*  .get('/bd/:curso/:periodo', (req, res) => {
-    const query = "SELECT D.nome, D.carga_horaria, CC.cod_comp_curricular from disciplina as D, componente_curricular as CC \
-        WHERE CC.cod_ppc = " + req.params.curso + " AND CC.periodo = " + req.params.periodo + " AND CC.cod_disciplina = D.cod_disciplina AND CC.cod_departamento = D.cod_departamento ORDER BY CC.cod_comp_curricular;"
-    
-    var dt = bdRoute.consultar( query, req.params.curso, req.params.periodo )   
-    console.log( "REST" + dt )
-    res.render('pages/home_construcao', { title: 'Seus putos', data : dt } )
-  })
-*/  //.get('/firebase', bdFireRoute )
-  //.get('/consulta', bdFireRoute )
   .use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
