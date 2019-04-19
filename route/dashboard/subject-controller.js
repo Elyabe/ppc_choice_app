91020afe56101ae947872e092649718f740b0929
elyabe <elly.ufes@gmail.com>
2019-04-19 20:16:09 -0300

    Inclusao dashboard > Disciplina

---
+++ route/dashboard/subject-controller.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../config/db-functions');
+const bodyParser = require('body-parser')
+const bcrypt = require('bcryptjs');
+
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
+
+const { ensureAuthenticated } = require('../../config/auth');
+const passport = require('passport');
+
+
+router.post( '/db/subject/', (req, res, next) => {
+}
+);
+
+router.get('/db/subject/', ensureAuthenticated, (req, res) => { 
+         var deptos = [], disciplinas = []; 
+     // res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    const get_qtd_disciplinas = "SELECT Disc.*, Dept.acronimo as acronimo_depto FROM disciplina as Disc, departamento as Dept WHERE Dept.cod_departamento = Disc.cod_departamento;";
+    const get_deptos = "SELECT * FROM departamento;";
+
+
+        db.getRecords( get_qtd_disciplinas, (result) => {
+          disciplinas = result.rows;
+          db.getRecords( get_deptos, (result) => {
+              deptos = result.rows;
+              res.render( './page/db/subject', { title: "PPC Choice - Dashboard", disciplinas: disciplinas, deptos:  deptos, user: req.user });
+          })
+        })
+
+});
+
+module.exports = router;
