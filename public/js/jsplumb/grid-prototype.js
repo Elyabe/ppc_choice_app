7d7d4b17ea9da4e8dbe2d1345e7b982662706222
elyabe <elly.ufes@gmail.com>
2019-05-03 13:46:42 -0300

    Implementação da create_grid

--- public/js/jsplumb/grid-prototype.js
+++ public/js/jsplumb/grid-prototype.js
@@ -18,7 +18,10 @@ $(document).ready( load_algorithm() );
 // Carrega todo o código pertinente
 function load_algorithm() {
         // Criar uma instancia do JSPlumb pra cada grade
-        // Chamar a função de montar a grade para cada uma das instâncias
+        // Chamar a função de montar a grpm ade para cada uma das instâncias
+        var target_grid_instance, current_grid_instance;
+        create_grid(current_grid_instance,current_grid,"current-grid");
+        create_grid(target_grid_instance,target_grid,"target-grid");
 }
 
 // Monta e exibe a grade de um curso 
@@ -27,6 +30,176 @@ function load_algorithm() {
 function  create_grid(instance, grid, container_name) 
 {
     // Ajustar a função "carregar" e colocar aqui
+    // canvas_current_grid
+    // canvas_target_grid
+    $("#cont-bar-progress-"+container_name).hide()
+    $("#canvas-"+container_name).hide()
+    $("#toggle-button-" + container_name).prop("disabled",true);   
+
+    $("#sl-"+container_name).change(function()
+        {
+            grid.clear();
+            var selected_course = $(this).children("option:selected").val();
+
+            if (selected_course > 0)
+            {
+                instance = create_instance_jsplumb(container_name);
+                $("#cont-bar-progress-"+container_name).show()
+                $("#bar-progress-"+container_name).show()
+                $("#bar-progress-"+container_name).css("width", "25%");
+                $("#"+container_name).empty();
+                $("#"+container_name).empty();
+
+                $.ajax({
+                    url: '/db/graduation/grid/' + selected_course,
+                    type:'GET',
+                    cache:true,
+                    success: function(response)
+                    {
+                        response.forEach( (item) => {
+                            grid.set( Number(item.cod_comp_curricular), item );
+                        })
+
+                        window.jsp = instance;
+
+                        var canvas = document.getElementById(container_name);
+                        var windows = jsPlumb.getSelector(".statemachine-demo .w");
+
+                        $.ajax({
+                            url: '/db/graduation/dependency/' + selected_course,
+                            type:'GET',
+                            cache:true,
+                            success: function(response)
+                            {
+                                instance.batch( function() {
+                                    /*for (i = 0; i < windows.length; i++) {
+                                        initNode(instance, windows[i], true);
+                                    }*/
+
+                                    var last_Period = 0, pos_subject_col=0;
+
+                                    for (var [cod, subject] of grid ) 
+                                    {
+                                        //depois trocar periodo por period
+                                        //periodo eh referente ao BD
+                                        if ( last_Period == subject.periodo ) 
+                                        {
+                                            pos_subject_col++
+                                        }else
+                                        {
+                                            pos_subject_col = 0;
+                                        };
+                                        last_Period = subject.periodo;
+                                        create_curricular_component(instance, subject, pos_subject_col )
+                                    }
+
+                                    if(container_name == 'current_grid')
+                                    {
+                                        cc_selected.clear();
+                                        grid.forEach( (item) =>{
+                                            //mudar cod_comp_curricular dps que mexer no bd
+                                            let curricomp = document.getElementById(item.cod_comp_curricular);
+
+                                            instance.on(curricomp, "click", function(e) {
+                                                if (cc_selected.has( Number(item.cod_comp_curricular)))
+                                                {
+                                                    cc_selected.delete( Number(item.cod_comp_curricular) );
+                                                    $("#"+item.cod_comp_curricular).css("background-color","white");
+                                                    $("#"+item.cod_comp_curricular).css("color","gray");   
+                                                    $("#"+item.cod_comp_curricular).removeClass('selected');
+                                                }
+                                                else
+                                                {
+                                                    var pre_requisites = [];
+                                                    pre_requisites.push(item.cod_comp_curricular);
+                                                    for ( var j = 0; j < pre_requisites.length; j++ )
+                                                    {
+                                                        response.filter( (item) => {
+                                                            return item.cod_comp_curricular == pre_requisites[j];
+                                                        }).forEach( (item) => { pre_requisites.push(item.cod_cc_pre_requisito) })
+                                                    }
+                                                    pre_requisites.forEach( (item) => {
+                                                        cc_selected.set(Number(item), grid.get(Number(item)));
+                                                        $("#"+item).addClass('selected');
+                                                    })
+
+                                                    console.log(pre_requisites);
+
+                                                    if (pre_requisites.length > 1)
+                                                    {
+                                                        const alert = '<div class="alert alert-warning alert-dismissible" role="alert">\
+                                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
+                                                            <strong>Opa!</strong> Imaginamos que vc já tenha cursado algumas disciplinas e as selecionamos automaticamente. \
+                                                            <br> Você pode retirar a seleção, se for o caso. \
+                                                            </div>';
+
+                                                        $('#canvas-current-grid').prepend(alert)
+                                                        setTimeout( function(){
+                                                            $('.alert').fadeOut(300, () => $(this).remove() );
+                                                        }, 5000 )
+                                                    }
+
+                                                }
+                                                //chamada da função comparar
+                                            }  )
+                                        })
+
+                                    }
+
+                                    grid.forEach( (item) => {
+                                        //mudar cod_comp_curricular dps q mexer no db
+                                        let curricomp = document.getElementById(item.cod_comp_curricular);
+                                     
+                                        instance.on(curricomp, "mouseover", function(e) {
+                                            instance.select({"source": item.cod_comp_curricular}).setHover(true);
+                                            instance.select({"target": item.cod_comp_curricular}).setHover(true);
+                                        });
+
+                                        instance.on(curricomp, "mouseout", function(e) {
+                                            instance.select({"source": item.cod_comp_curricular}).setHover(false);
+                                            instance.select({"target": item.cod_comp_curricular}).setHover(false);
+                                        });
+                                    })
+                                    
+                                    $("#bar-progress-"+container_name).css("width", "75%");
+                                    $("#bar-progress-"+container_name).css("width", "100%");
+                                    $("#bar-progress-"+container_name).hide()
+                                    $("#cont-bar-progress-"+container_name).hide()
+                                    
+                                    $("#canvas-"+container_name).addClass('in')
+                                    $("#canvas-"+container_name).css("height","")
+                                    $('#canvas-' + container_name).show()
+
+                                    // console.log(response)
+                                    response.forEach( (dep) =>
+                                    {
+                                        instance.connect({ source: dep.cod_cc_pre_requisito, target: dep.cod_comp_curricular, type:"basic" })
+                                    })
+
+                                    $("#toggle-button-" + container_name).prop("disabled",false);
+
+                                    let foco = ( container_name == 'current-grid') ? 'target-grid' : 'current-grid';
+                                    $('body, html').animate({
+                                        scrollTop: $("#sl-" + foco).offset().top
+                                    }, 600);
+
+                                    $("#sl-"+ foco).focus();
+
+                                })
+                            } 
+                        })
+                    }
+                })
+            }   
+            else
+            {
+                $('#' + container_name).empty();
+                $('#canvas-' + container_name).hide();
+                $("#toggle-button-" + container_name).prop("disabled",true);
+
+            }
+        })
+
 }   
 
 // Realiza comparação entre as grades dos curso e exibe ao usuário.
@@ -105,9 +278,9 @@ function create_curricular_component(instance, data, num )
     d.id = id;
     d.innerHTML = data.nome + "<br>(" + data.carga_horaria + " horas)";
     d.style.left = (data.periodo - 1)*140 + "px";
-    d.style.top = num*85 + "px";
+    d.style.top = num * 85 + "px";
     instance.getContainer().appendChild(d);
-    initNode(instance, d);
+    initialize_component(instance, d);
     return d;
 };
 
